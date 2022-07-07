b7b0668f1be00a3a6d367a65ba640b09d36ad478
p0d4r14n <p0d4r14n@protonmail.com>
2022-07-07 09:58:23 +0200

    update telescope - added fzf and file_browser

--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -4,3 +4,5 @@ local map = vim.keymap
 map.set("n", "<Leader>td", "<Cmd>Telescope diagnostics<CR>")
 map.set("n", "<Leader>tff", "<Cmd>Telescope find_files<CR>")
 map.set("n", "<Leader>tgf", "<Cmd>Telescope git_files<CR>")
+map.set("n", "<Leader>tlg", "<Cmd>Telescope live_grep<CR>")
+map.set("n", "<Leader>tfb", "<Cmd>Telescope file_browser<CR>")
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -2,7 +2,7 @@ local fn = vim.fn
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 
 local function get_config(name)
-  return string.format('require("config/%s")', name)
+  return string.format("require('config/%s')", name)
 end
 
 if fn.empty(fn.glob(install_path)) > 0 then
@@ -32,10 +32,12 @@ packer.startup(function(use)
 	use({ "wbthomason/packer.nvim" })
 	use({ "neovim/nvim-lspconfig", config = get_config("lsp") })
 	use {
-	  'nvim-telescope/telescope.nvim',
-	  requires = { {'nvim-lua/plenary.nvim'} },
+	  "nvim-telescope/telescope.nvim",
+	  requires = { {"nvim-lua/plenary.nvim"} },
 	  config = get_config("telescope"),
 	}
+	use {"nvim-telescope/telescope-fzf-native.nvim", run = "make" }
+	use { "nvim-telescope/telescope-file-browser.nvim" }
 	if packer_bootstrap then
 		packer.sync()
 	end
